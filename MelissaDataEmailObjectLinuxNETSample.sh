3cdfc8772f88252f139bc435794495a0079ec4d5
Melissa <root@DB-Erik10.melissadata.com>
2023-01-25 09:47:14 -0800

    Tested successfully using Melissa data files for 2023.01

--- MelissaDataEmailObjectLinuxNETSample.sh
+++ MelissaDataEmailObjectLinuxNETSample.sh
@@ -43,7 +43,7 @@ done
 
 ######################### Config ###########################
 
-RELEASE_VERSION='2022.12'
+RELEASE_VERSION='2023.01'
 ProductName="DQ_EMAIL_DATA"
 
 # Uses the location of the .ps1 file 
