b7d3a3880328a50b37e0abeaa23b591ad1a1b4a0
Melissa <>
2023-10-19 10:21:08 -0700

    Tested successfully using Melissa data files for 2023.10

--- MelissaEmailObjectLinuxDotnet.sh
+++ MelissaEmailObjectLinuxDotnet.sh
@@ -43,7 +43,7 @@ done
 
 ######################### Config ###########################
 
-RELEASE_VERSION='2023.09'
+RELEASE_VERSION='2023.10'
 ProductName="DQ_EMAIL_DATA"
 
 # Uses the location of the .ps1 file 
